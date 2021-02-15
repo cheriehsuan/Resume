0ba642e84f27ff15565e9e659b1bfc3ec7466ddd
Lin, Chia-Hsuan <cheriehsuan@gmail.com>
2021-02-15 18:57:30 +0800

    Site updated: 2021-02-15 18:57:29

---
+++ download/DLL_install/reg.bat
@@ -0,0 +1,4 @@
+C:\Windows\SysWOW64\regsvr32.exe "D:\ASVT\Program_software\DLL_install\COMDLG32.OCX"
+C:\Windows\SysWOW64\regsvr32.exe "D:\ASVT\Program_software\DLL_install\wodCrypt.dll"
+C:\Windows\SysWOW64\regsvr32.exe "D:\ASVT\Program_software\DLL_install\AVT_Cipher.dll"
+
